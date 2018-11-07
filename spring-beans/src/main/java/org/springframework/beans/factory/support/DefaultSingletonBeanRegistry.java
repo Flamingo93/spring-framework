ab5c7ed22a568081adca8ad619cc8b672ee4865f
常智超 <changzhichao@shandagames.com>
2018-11-07 11:49:13 +0800

    Add 注释：原始对象 所谓的原始对象是指刚创建好的对象，但还未调用 setter 填充属性就好比只有一个空壳子。 所谓的“早期引用”是指向原始对象的引用。

--- spring-beans/src/main/java/org/springframework/beans/factory/support/DefaultSingletonBeanRegistry.java
+++ spring-beans/src/main/java/org/springframework/beans/factory/support/DefaultSingletonBeanRegistry.java
@@ -71,12 +71,15 @@ import org.springframework.util.StringUtils;
 public class DefaultSingletonBeanRegistry extends SimpleAliasRegistry implements SingletonBeanRegistry {
 
 	/** Cache of singleton objects: bean name to bean instance. */
+	//用于存放完全初始化好的 bean，从该缓存中取出的 bean 可以直接使用
 	private final Map<String, Object> singletonObjects = new ConcurrentHashMap<>(256);
 
 	/** Cache of singleton factories: bean name to ObjectFactory. */
+	//存放 bean 工厂对象（ObjectFactory），用于解决循环依赖
 	private final Map<String, ObjectFactory<?>> singletonFactories = new HashMap<>(16);
 
 	/** Cache of early singleton objects: bean name to bean instance. */
+    //存放原始的 bean 对象（只是调用了new 构造函数，尚未填充属性调用setter），用于解决循环依赖
 	private final Map<String, Object> earlySingletonObjects = new HashMap<>(16);
 
 	/** Set of registered singletons, containing the bean names in registration order. */
